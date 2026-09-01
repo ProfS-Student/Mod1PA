2eba2b8bbcf197680416aa3081d69ce1d6b2b1ad
ProfS-Student <92692970+ProfS-Student@users.noreply.github.com>
2026-08-31 20:52:42 -0400

    module 1 solution

---
+++ module1.py
@@ -0,0 +1,22 @@
+# PalmerPenguins.py
+# D. Singletary
+# 8/31/26
+# Introduces Palmer Penguin Species
+
+# constants representing the species
+SP_CHINSTRAP = "Chinstrap"
+SP_GENTOO = "Gentoo"
+SP_ADELIE = "Adelie"
+
+TOTAL_SPECIES = 3
+
+# output the species names with introductory text
+print("Introducing the Palmer Penguins:")
+print("\t" + SP_CHINSTRAP + "!")
+print("\t" + SP_GENTOO + "!")
+print("and last but not least...")
+print("\t" + SP_ADELIE + "!")
+print()
+print("There are a total of " +
+      str(TOTAL_SPECIES) +
+      " penguin species in this dataset.")
